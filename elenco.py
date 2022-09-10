592c2941c6c9f6ad5388fe5347a65db784b298ec
glo18 <glaide.lara@gmail.com>
2022-09-10 03:10:22 -0300

    descrição do que tem que fazer

--- elenco.py
+++ elenco.py
@@ -1,36 +1,80 @@
-from re import L
+#!/usr/bin/python3
 import sys
+# variavel com o valor solucao otimo
+OPT = sys.maxsize 
 
-OPT = sys.maxsize
+#conjunto de atores com solucao otima
 Xopt = []
 atores = []
-CONTADOR = 0
-l = 0      # num de grupos
-m = 0        # num de atores
-n = 0        # num de personagens
+
+l = 0       # num de grupos
+m = 0       # num de atores
+n = 0       # num de personagens
+count = 0
 # argumentos:
 LIMITANTE_DADA    = False # usar função limitante dos profs.
 C_VIABILIDADE   = True  # cortes de viabilidade
 C_OTIMALIDADE   = True  # cortes de otimalidade
-
 class Ator:
-    """atributos: valor, grupos na qual pertence e indice dos mesmos"""
-    def __init__(self, valor,indice, grupos):
+    def __init__(self, valor, grupos, id):
         self.valor = valor
-        self.g_indice = indice
         self.grupos = grupos
+        self.id = id
 
     def __repr__(self):
-        return str(self.g_indice)
+        return str(self.id)
 
     def __str__(self):
-        return str(self.g_indice)
+        return str(self.id)
+
 
 def main():
     """inicio com a leitura"""
+
     leitura()
+    a_escolhidos = []
+    # se eh a funcao dos alunos, faz uma ordenacao nos valores
+    if not LIMITANTE_DADA:
+      atores.sort(key=lambda x: x.valor)
+    busca_elenco(a_escolhidos, atores)
+
+def busca_elenco(a_escolhidos, a_faltam):
+    # verifica se é viavel
+    (viavel, nova_solucao_otima) = viabilidade()
+    # se é viavel, atualiza os valores da solucao
+    if viavel:
+        if nova_solucao_otima < OPT:
+            OPT = nova_solucao_otima
+            Xopt = a_escolhidos
+    # nao tem mais atores para verificar
+    if not len(atores):
+      return
+    
+    # verifica se há corte de viabilidade
+    if C_VIABILIDADE:
+        # chama funcao de corte_viabilidade
+      
+    # verifica se ja corte de otimalidade
+    if C_OTIMALIDADE:
+        # verifica qual é a funcao limitante
+        if LIMITANTE_DADA:
+            # chama funcao do professor
+        else:
+            # faz com funcao dos alunos
+        if (v_atual >= OPT):
+           return
+    # ainda falta escolher atores        
+    e = a_escolhidos.copy()
+    f = a_faltam.copy()
+    ator = f.pop(0)
+    # decido não escolher o próximo ator
+    busca_elenco(e,f)
+    # decido escolher o próximo ator
+    e.append(ator)
+    busca_elenco(e,f)
 
 
+# def viabilidade():
 
 def leitura():
     """"Lê todos os dados de entrada"""
@@ -50,7 +94,8 @@ def leitura():
         grupos = []
         for j in range(ator_s):
             grupos.append(j+1)
-        atores.append(Ator(ator_v, ator_s, grupos))   
+        atores.append(Ator(ator_v,  grupos, i+1))  
+
 
 if __name__ == "__main__":
     for arg in sys.argv:
@@ -60,5 +105,4 @@ if __name__ == "__main__":
             C_VIABILIDADE = False
         if arg == "-o":
             C_OTIMALIDADE = False
-
     main()
